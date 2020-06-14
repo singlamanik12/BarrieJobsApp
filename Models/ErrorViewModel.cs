0ca7da1742f4454f1373de748ebe3a74846fc310
singlamanik12 <44194522+singlamanik12@users.noreply.github.com>
2020-06-14 03:11:45 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BarrieJobsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
